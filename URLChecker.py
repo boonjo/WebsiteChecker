2a42321b9c24a9170a55cb13ccf2036b41e5c00e
kim <nahappy15@naver.com>
2021-07-06 14:45:25 +0900

    url checker first commit

---
+++ URLChecker.py
@@ -0,0 +1,20 @@
+import requests
+import time
+
+class SendRequest:
+    def __init__(self):
+        self.makingList = MakeList()
+        self.URL_list = self.makingList.get_list()
+
+    def get_error_url(self, url_list):
+        error_url_list = []
+        for url in url_list:
+            try:
+                status_code = requests.get(url).status_code
+            except:
+                time.sleep(5)
+                error_url_list.append(url)
+                continue
+            if 400 <= status_code:
+                error_url_list.append(url)
+        return error_url_list
